4e53f714dc39ff83f5038446449bc1052b8f6fe0
Данила Зоткин <danilazotkin@MacBook-Pro-Danila.local>
2021-04-03 13:10:10 +0300

    PlayerController fix, add Camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,32 +6,43 @@ public class PlayerController : MonoBehaviour
 {
 
     [SerializeField] private float verticalSpeed = 5.0f;
-    [SerializeField] private float horizontalSpeed = 15.0f;
-    private Camera _camera;
-
+    [SerializeField] private float horizontalSpeed = 5.0f;
+    [SerializeField] private float fixedSpeed = 15.0f;
     [SerializeField] public Transform light;
+    [SerializeField] public float rotationLock = 45.0f;
+
+    private Camera _camera;
+    private float _horizontal = 0f;
+    private float _vertical = 0f;
+    private Vector3 _difference;
+    private Rigidbody2D _rb;
+
+  
 
     // Start is called before the first frame update
     void Start()
     {
+        _rb = GetComponent<Rigidbody2D>();
         _camera = Camera.main;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 transformPosition = transform.position;
-        transformPosition.x += horizontalSpeed * Time.deltaTime;
-        transformPosition.y += Input.GetAxis("Vertical") * verticalSpeed * Time.deltaTime;
-        transform.position = transformPosition;
-        if (_camera != null)
-        {
-            Vector3 worldPoint = _camera.ScreenToWorldPoint(Input.mousePosition);
-            Quaternion targetRotation = Quaternion.LookRotation(Vector3.forward, (worldPoint - transform.position));
-            //TODO constraints only +-45 grad
-            light.rotation = targetRotation;
-            
-            light.Rotate(0, 0, 90);
+        _horizontal = Input.GetAxis("Horizontal");
+        _vertical = Input.GetAxis("Vertical");
+        _difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        _difference.Normalize();
+    }
+
+    private void FixedUpdate() {
+        //movement
+        _rb.velocity = new Vector2(fixedSpeed * Time.fixedDeltaTime + _horizontal * horizontalSpeed * Time.fixedDeltaTime, _vertical * verticalSpeed * Time.fixedDeltaTime);
+        //light rotation
+        float rotation_z = Mathf.Atan2(_difference.y, _difference.x) * Mathf.Rad2Deg;
+        if(System.Math.Abs(rotation_z) <= rotationLock) {
+            light.rotation = Quaternion.Euler(0f, 0f, rotation_z);
         }
     }
+
 }
---
+++ Assets/Scripts/Prallax.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Prallax : MonoBehaviour
+{
+    [SerializeField] GameObject camera;
+    [SerializeField] float prallaxEffect = 5f;
+
+    private float _lenght;
+    private float _startPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startPosition = transform.position.x;
+        _lenght = GetComponent<SpriteRenderer>().bounds.size.x;
+        Debug.Log(_lenght);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float distance = (camera.transform.position.x * prallaxEffect);
+        float temp = (camera.transform.position.x * (1 - prallaxEffect));
+        transform.position = new Vector3(_startPosition + distance, transform.position.y, transform.position.z);
+
+        if (temp > distance + _lenght) {
+            _startPosition += _lenght;
+        }
+        else if (temp < distance - _lenght) {
+            _startPosition -= _lenght;
+        }
+
+    }
+}
