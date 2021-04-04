522090af5be59ac22a129275aca40a8d1b776517
Данила Зоткин <danilazotkin@MacBook-Pro-Danila.local>
2021-04-04 17:00:08 +0300

    Prallax changes v2

--- Assets/Scripts/backgroundPrallax.cs
+++ Assets/Scripts/backgroundPrallax.cs
@@ -6,46 +6,94 @@ public class backgroundPrallax : MonoBehaviour
 {
     [SerializeField] public GameObject bg_1;
     [SerializeField] public GameObject bg_2;
+    [SerializeField] public GameObject bg_1_2;
+    [SerializeField] public GameObject bg_2_2;
+    [SerializeField] public float xDeltaMult = -0.5f;
 
     [SerializeField] public Sprite[] main_bg;
     [SerializeField] public Sprite[] second_bg;
 
     private float _leftCameraBorder; //координаты левой границы камеры
+    private float _lastCameraPosition;
     private float _bg_1_end = 0.0f, _bg_2_end = 0.0f;
-    private SpriteRenderer _bg_1_renderer, _bg_2_renderer;
+    private SpriteRenderer _bg_1_renderer, _bg_2_renderer, _bg_1_2_renderer, _bg_2_2_renderer;
     private bool _mainLeft = true;
 
-    private void intilizeBackground(bool left)
+    private void intilizeBackground(bool left, bool first = false)
     {
         int bg_main_random = Random.Range(0, main_bg.Length);
-        if (left)
-        {
-            _bg_1_renderer.sprite = main_bg[0];
-            _bg_1_end = _mainLeft ? 0 : _bg_2_end;
-            _bg_1_end += _bg_1_renderer.sprite.bounds.extents.x * 2.0f;
-            bg_1.transform.position = new Vector3(_bg_2_end, 0.0f, 0.0f);
+        int bg_second_random = Random.Range(0, second_bg.Length);
 
-            _mainLeft = false;
-        }
-        else
+        if(first)
         {
-            _bg_2_renderer.sprite = main_bg[0];
-            _bg_2_end = !_mainLeft ? 0 : _bg_1_end;
-            _bg_2_end += _bg_2_renderer.sprite.bounds.extents.x * 2.0f;
-            bg_2.transform.position = new Vector3(_bg_1_end, 0.0f, 0.0f);
-            _mainLeft = true;
+            if(left) 
+            {
+                _bg_1_renderer.sprite = main_bg[bg_main_random];
+                _bg_1_2_renderer.sprite = second_bg[bg_second_random];
+
+                _bg_1_end = _mainLeft ? 0 : _bg_2_end;
+                _bg_1_end += _bg_1_renderer.sprite.bounds.extents.x * 3.0f + _leftCameraBorder;
+
+                bg_1.transform.position = new Vector3(_leftCameraBorder + _bg_1_renderer.sprite.bounds.extents.x, 0.0f, 0.0f);
+            } 
+            else 
+            {
+                bg_2.transform.position = new Vector3(_bg_1_end, 0.0f, 0.0f);
+
+                _bg_2_renderer.sprite = main_bg[bg_main_random];
+                _bg_2_2_renderer.sprite = second_bg[bg_second_random];
+
+                _bg_2_end = !_mainLeft ? 0 : _bg_1_end;
+                _bg_2_end += _bg_2_renderer.sprite.bounds.extents.x * 2.0f;   
+            }
+        } 
+        else
+        { 
+            if (left)
+            {
+                bg_1.transform.position = new Vector3(_bg_2_end, 0.0f, 0.0f);
+                bg_1_2.transform.position = new Vector3(_bg_2_end, 0.0f, 0.0f);
+
+                _bg_1_renderer.sprite = main_bg[bg_main_random];
+                _bg_1_2_renderer.sprite = second_bg[bg_second_random];
+                
+
+                _bg_1_end = _mainLeft ? 0 : _bg_2_end;
+                _bg_1_end += _bg_1_renderer.sprite.bounds.extents.x * 2.0f;
+
+                _mainLeft = false;
+            }
+            else
+            {
+                bg_2.transform.position = new Vector3(_bg_1_end, 0.0f, 0.0f);
+                bg_2_2.transform.position = new Vector3(_bg_1_end, 0.0f, 0.0f);
+
+                _bg_2_renderer.sprite = main_bg[bg_main_random];
+                _bg_2_2_renderer.sprite = second_bg[bg_second_random];
+
+                _bg_2_end = !_mainLeft ? 0 : _bg_1_end;
+                _bg_2_end += _bg_2_renderer.sprite.bounds.extents.x * 2.0f;
+                
+
+                _mainLeft = true;
+            }
         }
     }
 
 
-    private void Start()
+    private void Awake()
     {
         _bg_1_renderer = bg_1.GetComponent<SpriteRenderer>();
-        _bg_2_renderer = bg_2.GetComponent<SpriteRenderer>();
+        _bg_1_2_renderer = bg_1_2.GetComponent<SpriteRenderer>();
 
-        intilizeBackground(true);
-        _mainLeft = true;
-        intilizeBackground(false);
+        _bg_2_renderer = bg_2.GetComponent<SpriteRenderer>();
+        _bg_2_2_renderer = bg_2_2.GetComponent<SpriteRenderer>();
+
+        _leftCameraBorder = Camera.main.ScreenToWorldPoint(new Vector3(0, Camera.main.pixelHeight, 0)).x;
+        _lastCameraPosition = _leftCameraBorder;
+
+        intilizeBackground(true, true);
+        intilizeBackground(false, true);
     }
 
     private void Update()
@@ -53,8 +101,14 @@ public class backgroundPrallax : MonoBehaviour
         _leftCameraBorder = Camera.main.ScreenToWorldPoint(new Vector3(0, Camera.main.pixelHeight, 0)).x;
     }
 
-    private void FixedUpdate()
+    private void LateUpdate()
     {
+        float deltaMovement = _leftCameraBorder - _lastCameraPosition;
+        _lastCameraPosition = _leftCameraBorder;
+
+        bg_1_2.transform.position += new Vector3(deltaMovement * xDeltaMult, 0.0f, 0.0f);
+        bg_2_2.transform.position += new Vector3(deltaMovement * xDeltaMult, 0.0f, 0.0f);
+
         if (_leftCameraBorder > _bg_1_end)
         {
             intilizeBackground(true);
